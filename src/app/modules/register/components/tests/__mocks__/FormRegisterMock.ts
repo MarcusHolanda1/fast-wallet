a1b1c8706d3720ffeb22af483c5be09fe1d112d0
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-05 18:57:14 -0300

    test: add mock data and response for FormRegisterCard unit tests

---
+++ src/app/modules/register/components/tests/__mocks__/FormRegisterMock.ts
@@ -0,0 +1,19 @@
+import { Card } from '@app/modules/register/types/card';
+
+export const generateFakeCardData = (): Card => {
+  return {
+    id: '1',
+    number: '4111111111111111',
+    cvv: '333',
+    name: 'MARCUS HOLANDA',
+    expires: '12/30'
+  };
+};
+
+export const mockCreateCardResponse = (
+  cardData: Card
+): Promise<{ status: number; data: Card }> =>
+  Promise.resolve({
+    status: 200,
+    data: cardData
+  });
