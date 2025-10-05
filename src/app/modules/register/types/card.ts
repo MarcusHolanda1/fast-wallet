caa22f1ebfb24c8cb3e52b29e6426f3a6dd5c628
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-05 18:58:14 -0300

    refactor: remove getCards service and add expires field to Card and CardPayload types

--- src/app/modules/register/types/card.ts
+++ src/app/modules/register/types/card.ts
@@ -3,10 +3,12 @@ export interface Card {
   number: string;
   cvv: string;
   name: string;
+  expires: string;
 }
 
 export interface CardPayload {
   number: string;
   cvv: string;
   name: string;
+  expires: string;
 }
