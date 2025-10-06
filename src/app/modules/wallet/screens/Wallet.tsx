69fda201edc4428bffb74d2162906b0a21d3b525
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-06 14:17:15 -0300

    feat: enhance WalletScreen layout with CreditCard component and improved styles

--- src/app/modules/wallet/screens/Wallet.tsx
+++ src/app/modules/wallet/screens/Wallet.tsx
@@ -1,33 +1,62 @@
+import { Text, View, StyleSheet } from 'react-native';
 import { useAppSelector } from '@app/store/hooks';
-import { Text, View } from 'react-native';
 import PageContainer from '@app/shared/components/containers/PageContainer';
+import CreditCard from '@app/shared/components/cards/CreditCard';
+import { theme } from '@app/shared/theme/theme';
 
 import useGetCards from '../hooks/useGetCards';
 
 export default function WalletScreen() {
   const { isLoading } = useGetCards();
-
   const cards = useAppSelector((state) => state.cards.cards);
+
+  const cardColors = [theme.colors.base.greenLight, theme.colors.text.black];
 
   if (isLoading) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.center}>
         <Text>Carregando...</Text>
       </View>
     );
   }
 
   return (
-    <PageContainer showWalletHeader={true} showTitle={false}>
-      <Text>Meus cartões</Text>
-
-      {cards.map((card) => (
-        <View key={card.id} style={{ marginTop: 20 }}>
-          <Text>Número: {card.number}</Text>
-          <Text>Nome: {card.name}</Text>
-          <Text>Validade: {card.expires}</Text>
-        </View>
-      ))}
+    <PageContainer showWalletHeader showTitle={false}>
+      <View style={styles.walletContainer}>
+        {cards &&
+          cards.map((card, index) => {
+            const offset = index * -112;
+            const backgroundColor = cardColors[index % cardColors.length];
+            return (
+              <View
+                key={card.id}
+                style={[{ marginTop: offset, zIndex: cards.length + index }]}
+              >
+                <CreditCard
+                  card={card}
+                  backgroundColor={backgroundColor}
+                  textColor={
+                    backgroundColor === theme.colors.text.black
+                      ? theme.colors.base.white
+                      : theme.colors.text.black
+                  }
+                />
+              </View>
+            );
+          })}
+      </View>
     </PageContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  walletContainer: {
+    marginTop: 20,
+    position: 'relative'
+  }
+});
