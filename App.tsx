6a2cede5afa9f1341f80751c0bbfbb6dd039b5ad
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-06 01:57:38 -0300

    feat: wrap application in Redux Provider for state management

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import { store } from '@app/store';
 import { Routes } from '@shared/navigation/Router';
+import { Provider } from 'react-redux';
 import ToastManager from 'toastify-react-native';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routes />
       <ToastManager />
-    </>
+    </Provider>
   );
 }
