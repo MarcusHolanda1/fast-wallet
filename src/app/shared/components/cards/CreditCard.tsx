d86ccc143bd6d4fc1922291b7459c5a360561eab
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-06 01:59:44 -0300

    feat: add CreditCard component for displaying card details

---
+++ src/app/shared/components/cards/CreditCard.tsx
@@ -0,0 +1,79 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { theme } from '@shared/theme/theme';
+
+interface CreditCardProps {
+  cardholderName: string;
+  cardNumber: string;
+  expiryDate: string;
+  cardType: string;
+  backgroundColor: string;
+  textColor: string;
+}
+
+export default function CreditCard({
+  cardholderName,
+  cardNumber,
+  expiryDate,
+  cardType,
+  backgroundColor,
+  textColor
+}: CreditCardProps) {
+  return (
+    <View style={[styles.cardContainer, { backgroundColor: backgroundColor }]}>
+      <Text style={styles.cardType}>{cardType}</Text>
+
+      <View style={styles.cardContent}>
+        <Text style={[styles.cardholderName, { color: textColor }]}>
+          {cardholderName}
+        </Text>
+        <Text style={[styles.cardNumber, { color: textColor }]}>
+          {cardNumber}
+        </Text>
+        <Text style={[styles.expiryDate, { color: textColor }]}>
+          Validade {expiryDate}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    height: 180,
+    borderRadius: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 30,
+    justifyContent: 'space-between',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 8,
+    elevation: 8
+  },
+  cardType: {
+    fontSize: theme.typography.h5.fontSize,
+    fontWeight: '600',
+    marginBottom: 3
+  },
+  cardContent: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  cardholderName: {
+    fontSize: theme.typography.p.fontSize,
+    fontWeight: '500',
+    marginBottom: 3
+  },
+  cardNumber: {
+    fontSize: 18,
+    fontWeight: '600',
+    letterSpacing: 2
+  },
+  expiryDate: {
+    fontSize: 14,
+    fontWeight: '400'
+  }
+});
