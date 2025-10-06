d06a52880c3bed98db96513e878ea436b77dcfd8
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-06 01:58:05 -0300

    feat: create mock data generator for Card in registerMocks

--- src/app/modules/register/components/tests/__mocks__/FormRegisterMock.ts
+++ src/app/modules/register/components/tests/__mocks__/FormRegisterMock.ts
@@ -1,15 +1,5 @@
 import { Card } from '@app/shared/types/card';
 
-export const generateFakeCardData = (): Card => {
-  return {
-    id: '1',
-    number: '4111111111111111',
-    cvv: '333',
-    name: 'MARCUS HOLANDA',
-    expires: '12/30'
-  };
-};
-
 export const mockCreateCardResponse = (
   cardData: Card
 ): Promise<{ status: number; data: Card }> =>
