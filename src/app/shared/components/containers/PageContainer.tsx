d1b32ec73ce4ef8c12b597f7c525f9a98d18ee45
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-06 12:52:14 -0300

    feat: add showWalletHeader prop to PageContainer for optional wallet header display

--- src/app/shared/components/containers/PageContainer.tsx
+++ src/app/shared/components/containers/PageContainer.tsx
@@ -4,11 +4,22 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 interface PageContainerProps {
   children: React.ReactNode;
   showTitle?: boolean;
+  showWalletHeader?: boolean;
 }
 
-const PageContainer = ({ children, showTitle = true }: PageContainerProps) => {
+const PageContainer = ({
+  children,
+  showTitle = true,
+  showWalletHeader = false
+}: PageContainerProps) => {
   return (
     <SafeAreaView style={styles.safeArea}>
+      {showWalletHeader && (
+        <View style={styles.walletHeader}>
+          <Text style={styles.myWalletText}>Meus cartões</Text>
+        </View>
+      )}
+
       <View style={styles.container}>
         {showTitle && <Text style={styles.title}>Wallet Test</Text>}
         {children}
@@ -34,6 +45,18 @@ const styles = StyleSheet.create({
     fontWeight: 'regular',
     color: theme.colors.base.white,
     marginBottom: 30
+  },
+  walletHeader: {
+    height: 66,
+    backgroundColor: theme.colors.base.white,
+    borderBottomEndRadius: 50,
+    borderBottomStartRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  myWalletText: {
+    ...theme.typography.h4,
+    color: theme.colors.base.blueLight
   }
 });
 
