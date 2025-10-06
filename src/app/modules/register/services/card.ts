3d5bae51bf5fe13cfbb3c00eb5f5bfdc7ef028e9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-06 01:05:45 -0300

    refactor: move Card interface to shared types and update imports in card services

--- src/app/modules/register/services/card.ts
+++ src/app/modules/register/services/card.ts
@@ -1,8 +1,8 @@
 import { httpClient } from '@app/shared/services/httpClient/httpClient';
 import { HttpResponse } from '@app/shared/services/httpClient/types';
 
-import { CARDS } from '../constants/endpoints';
-import { Card } from '../types/card';
+import { CARDS } from '../../../shared/constants/endpoints';
+import { Card } from '../../../shared/types/card';
 
 export const createCard = async (payload: {
   number: string;
