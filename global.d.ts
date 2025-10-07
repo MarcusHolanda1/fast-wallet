b9ec94d89da169b60f8e4824fb7b160624cd1244
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-07 04:14:35 -0300

    feat: add TypeScript support for SVG imports and update path aliases in tsconfig

--- global.d.ts
+++ global.d.ts
@@ -5,3 +5,10 @@ declare global {
 }
 
 export { __global };
+
+declare module '*.svg' {
+  import React from 'react';
+  import { SvgProps } from 'react-native-svg';
+  const content: React.FC<SvgProps>;
+  export default content;
+}
