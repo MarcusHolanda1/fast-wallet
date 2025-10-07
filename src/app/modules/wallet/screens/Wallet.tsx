4a51099139ffa34fdfcb15812119314c0929fbae
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-07 02:56:14 -0300

    feat: enhance WalletScreen with loading state management and WalletLoading component

--- src/app/modules/wallet/screens/Wallet.tsx
+++ src/app/modules/wallet/screens/Wallet.tsx
@@ -1,30 +1,43 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { useAppSelector } from '@app/store/hooks';
 import PageContainer from '@app/shared/components/containers/PageContainer';
 import CreditCard from '@app/shared/components/cards/CreditCard';
 import { theme } from '@app/shared/theme/theme';
+import { JSX, useEffect, useState } from 'react';
+import { useAppNavigation } from '@app/shared/hooks/useNavigation';
 
 import useGetCards from '../hooks/useGetCards';
+import WalletLoading from '../components/WalletLoading';
 
-export default function WalletScreen() {
-  const { isLoading } = useGetCards();
+export default function WalletScreen(): JSX.Element {
+  const [delayLoading, setDelayLoading] = useState<boolean>(true);
+
   const cards = useAppSelector((state) => state.cards.cards);
+  const { isLoading } = useGetCards();
+  const navigation = useAppNavigation();
+
+  useEffect(() => {
+    const t = setTimeout(() => setDelayLoading(false), 3000);
+    return () => clearTimeout(t);
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: !(isLoading || delayLoading)
+    });
+  }, [isLoading, delayLoading, navigation]);
 
   const cardColors = [theme.colors.base.greenLight, theme.colors.text.black];
 
-  if (isLoading) {
-    return (
-      <View style={styles.center}>
-        <Text>Carregando...</Text>
-      </View>
-    );
+  if (isLoading || delayLoading) {
+    return <WalletLoading />;
   }
 
   return (
     <PageContainer
       showWalletHeader
       showTitle={false}
-      showBackgroundImages={false}
+      showBackgroundRectangle={false}
     >
       <View style={styles.walletContainer}>
         {cards &&
@@ -60,7 +73,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   walletContainer: {
-    marginTop: 20,
     position: 'relative'
   }
 });
