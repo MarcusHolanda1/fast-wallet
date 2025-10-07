1d5292c72c9f7a28b75c1b1ca3b51ccbea88b71e
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-07 02:56:33 -0300

    feat: replace background images with BackgroundRectangle component in PageContainer

--- src/app/shared/components/containers/PageContainer.tsx
+++ src/app/shared/components/containers/PageContainer.tsx
@@ -1,39 +1,34 @@
 import { theme } from '@app/shared/theme/theme';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import BackgroundTopAppSvg from '@assets/svgs/background-top-app.svg';
-import BackgroundBottomAppSvg from '@assets/svgs/background-bottom-app.svg';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import BackgroundRectangle from '@app/modules/wallet/components/BackgroundRectangle';
+import useResponsiveBackgroundRectangles from '@app/shared/hooks/useResponsiveBackgroundRectangles';
 
 interface PageContainerProps {
   children: React.ReactNode;
   showTitle?: boolean;
   showWalletHeader?: boolean;
-  showBackgroundImages?: boolean;
+  showBackgroundRectangle?: boolean;
 }
 
 const PageContainer = ({
   children,
   showTitle = true,
-  showWalletHeader = false,
-  showBackgroundImages = true
+  showBackgroundRectangle = true
 }: PageContainerProps) => {
+  const { topStyle, bottomStyle } = useResponsiveBackgroundRectangles();
   return (
     <SafeAreaView style={styles.safeArea}>
-      {showBackgroundImages && (
-        <BackgroundTopAppSvg style={styles.backgroundTopSvg} />
-      )}
-
-      {showWalletHeader && (
-        <View style={styles.walletHeader}>
-          <Text style={styles.myWalletText}>Meus cartões</Text>
-        </View>
+      {showBackgroundRectangle && (
+        <BackgroundRectangle style={topStyle} rotation="-34deg" />
       )}
 
       <View style={styles.container}>
         {showTitle && <Text style={styles.title}>Wallet Test</Text>}
         {children}
       </View>
-      {showBackgroundImages && (
-        <BackgroundBottomAppSvg style={styles.backgroundBottomSvg} />
+      {showBackgroundRectangle && (
+        <BackgroundRectangle style={bottomStyle} rotation="-220deg" />
       )}
     </SafeAreaView>
   );
@@ -43,25 +38,6 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: theme.colors.base.blueDark
-  },
-  backgroundTopSvg: {
-    position: 'absolute',
-    top: -172,
-    left: -129,
-    right: 0,
-    bottom: 0,
-    width: '100%',
-    height: '100%',
-    zIndex: 1
-  },
-  backgroundBottomSvg: {
-    position: 'absolute',
-    top: 630,
-    left: 130,
-    right: 0,
-    bottom: 0,
-    width: '100%',
-    height: '100%'
   },
   container: {
     flex: 1,
@@ -75,18 +51,6 @@ const styles = StyleSheet.create({
     fontWeight: 'regular',
     color: theme.colors.base.white,
     marginBottom: 30
-  },
-  walletHeader: {
-    height: 66,
-    backgroundColor: theme.colors.base.white,
-    borderBottomEndRadius: 50,
-    borderBottomStartRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  myWalletText: {
-    ...theme.typography.h4,
-    color: theme.colors.base.blueLight
   }
 });
 
