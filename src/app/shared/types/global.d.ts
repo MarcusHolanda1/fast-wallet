698a7623e741fb470f57de1917718f29b8ca763e
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-07 18:59:18 -0300

    feat: add Jest mocks for SafeAreaContext and global navigation functions, and update TypeScript definitions for SVG modules

--- src/app/shared/types/global.d.ts
+++ src/app/shared/types/global.d.ts
@@ -4,11 +4,11 @@ declare global {
   const mockGoBack: jest.Mock;
 }
 
-export { __global };
-
 declare module '*.svg' {
   import React from 'react';
   import { SvgProps } from 'react-native-svg';
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+export { __global };
