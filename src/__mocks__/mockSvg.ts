cc0a29758be836cc1a17556edc0e895673f552a7
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-08 13:13:52 -0300

    chore: update configuration files for improved TypeScript and ESLint support

--- src/__mocks__/mockSvg.ts
+++ src/__mocks__/mockSvg.ts
@@ -4,5 +4,7 @@ import { View, ViewProps } from 'react-native';
 const SvgMock = React.forwardRef<View, ViewProps>((props, ref) =>
   React.createElement(View, { ...props, ref, testID: 'svg-mock' })
 );
+// Added display name for ESLint react/display-name rule
+SvgMock.displayName = 'SvgMock';
 
 export default SvgMock;
