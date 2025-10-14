0bff765866145b0aec53a5190a7636e84d723063
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2025-10-14 17:43:16 -0300

    chore: update ESLint configuration to include unused-imports plugin and adjust various rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import eslint from '@eslint/js';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,7 +11,9 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname
+        tsconfigRootDir: import.meta.dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module'
       }
     }
   },
@@ -32,14 +35,76 @@ export default tseslint.config(
       }
     },
     plugins: {
-      'react-hooks': reactHooks
+      'react-hooks': reactHooks,
+      'unused-imports': unusedImports
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'no-console': 'warn',
+
+      '@typescript-eslint/camelcase': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': ['off', { ignoreRestArgs: false }],
+      '@typescript-eslint/indent': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+
+      'no-unused-vars': 'off',
+      camelcase: 'off',
+      'global-require': 'off',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-empty-function': 'off',
+      'no-console': 'off',
+      'no-duplicate-imports': 'error',
+      'no-multiple-empty-lines': 'error',
+      'no-underscore-dangle': 'off',
+      'no-param-reassign': 'off',
+      'no-await-in-loop': 'off',
+      'no-eval': 'off',
+      'no-restricted-syntax': 'off',
+      'linebreak-style': 'off',
+      semi: ['error', 'always'],
+
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ],
+
+      'react/display-name': 'off',
+      'react/prop-types': 'off',
+      'react/no-children-prop': 'off',
+      'react/function-component-definition': 'off',
+      'react/require-default-props': 'off',
+      'react/jsx-no-undef': 'off',
+      'react/jsx-no-bind': 'off',
+      'react/jsx-props-no-spreading': 'off',
       'react/jsx-no-useless-fragment': 'error',
-      'react-hooks/exhaustive-deps': 'off',
-      '@typescript-eslint/no-unused-vars': 'off'
+      'react/style-prop-object': [
+        'off',
+        {
+          allow: ['error']
+        }
+      ],
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.tsx']
+        }
+      ],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
     }
   },
   {
